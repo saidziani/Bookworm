93a4b58e2217ccc06a3369bc14267c1505c334e1
Said <saidziani.pro@gmail.com>
2019-08-03 18:42:05 +0200

    Adding graphene library and graphql schema path

--- server/app/settings.py
+++ server/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'graphene_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app.bookworm'
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'app.schema.schema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
