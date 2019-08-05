1e27945c582bdf96119c0ef5110830ef2160f3b7
Said <saidziani.pro@gmail.com>
2019-08-05 21:13:51 +0200

    Register User model

--- server/app/bookworm/admin.py
+++ server/app/bookworm/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import House, Writer, Category, Book
+from .models import House, Writer, Category, Book, User
 
 admin.site.register(House)
 admin.site.register(Writer)
 admin.site.register(Category)
 admin.site.register(Book)
+admin.site.register(User)
 
